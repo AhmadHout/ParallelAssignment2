a13cfad173cd54f16d45fd3e99559c2d4d274cae
AhmadHout <160398641+AhmadHout@users.noreply.github.com>
2024-03-21 01:01:03 +0200

    pthreads.c

--- pthreads.c
+++ pthreads.c
@@ -3,97 +3,86 @@
 #include <pthread.h>
 #include <time.h>
 
-#define ARRAY_SIZE 100
-#define NUM_BUCKETS 10
-#define NUM_THREADS 4
+#define MAX_VALUE 1000
+#define NUM_BUCKETS 100
+
 
 typedef struct {
-    int *array;
-    int start;
-    int end;
-    int **buckets;
-} ThreadData;
+    int* arr;         
+    int* bucket;       
+    int start;         
+    int end;           
+} specs;
 
-int compareIntegers(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
-}
-
-void *insertIntoBuckets(void *arg) {
-    ThreadData *data = (ThreadData *)arg;
-    int *array = data->array;
-    int start = data->start;
-    int end = data->end;
-    int **buckets = data->buckets;
-
-    for (int i = start; i < end; i++) {
-        int bucket_index = array[i] / (ARRAY_SIZE / NUM_BUCKETS);
-        int index = ++buckets[bucket_index][0];
-        buckets[bucket_index][index] = array[i];
+void* buckethelper(void* arg) {
+    specs* info = (specs*)arg;
+    for (int i = info->start; i < info->end; i++) {
+        info->bucket[info->arr[i]]++;
     }
-
-    pthread_exit(NULL);
+    return NULL;
 }
+void bucketSort(int arr[], int size) {
+    
+    int bucket[MAX_VALUE + 1] = {0};
+    int num_threads = 2; 
+    pthread_t threads[num_threads];
+    specs bucket_infos[num_threads];
 
-void gatherSortedElements(int *array, int **buckets) {
+
+    int chunk_size = size / num_threads;
+    for (int i = 0; i < num_threads; i++) {
+        bucket_infos[i].arr = arr;
+        bucket_infos[i].bucket = bucket;
+        bucket_infos[i].start = i * chunk_size;
+        if (i == num_threads - 1) {
+            bucket_infos[i].end = size;
+        } else {
+            bucket_infos[i].end = (i + 1) * chunk_size;
+        }
+        pthread_create(&threads[i], NULL, buckethelper, &bucket_infos[i]);
+    }
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
     int index = 0;
-    for (int i = 0; i < NUM_BUCKETS; i++) {
-        for (int j = 1; j <= buckets[i][0]; j++) {
-            array[index++] = buckets[i][j];
+    for (int i = 0; i <= MAX_VALUE; i++) {
+        for (int j = 0; j < bucket[i]; j++) {
+            arr[index++] = i;
         }
     }
 }
 
-void bucketsort(int arr[], int length) {
-    int maximum = arr[0];
-    for (int i = 1; i < length; i++) {
-        if (arr[i] > maximum)
-            maximum = arr[i];
+int main() {
+    srand(time(NULL));
+    int* arr = malloc(NUM_BUCKETS* sizeof(int));
+    if (arr == NULL) {
+        perror("Memory allocation failed");
+        return EXIT_FAILURE;
     }
 
-    int **buckets = (int **)malloc(sizeof(int *) * NUM_BUCKETS);
-    for (int i = 0; i < NUM_BUCKETS; i++) {
-        buckets[i] = (int *)malloc(sizeof(int) * ARRAY_SIZE);
-        buckets[i][0] = 0; 
+    
+    for (int i = 0; i<NUM_BUCKETS; i++) {
+        arr[i] = rand() % MAX_VALUE;
     }
-    pthread_t threads[NUM_THREADS];
-    ThreadData threadData[NUM_THREADS];
 
-    for (int i = 0; i < NUM_THREADS; i++) {
-        threadData[i].array = arr;
-        threadData[i].start =i*(length/NUM_THREADS);
-        threadData[i].end = (i + 1)*(length/NUM_THREADS);
-        threadData[i].buckets = buckets;
-        pthread_create(&threads[i], NULL,insertIntoBuckets,&threadData[i]);
-    }
-    //joining threadsd
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    //gather
-    gatherSortedElements(arr, buckets);
+    
+    clock_t start = clock();
+
+  
+    bucketSort(arr, NUM_BUCKETS);
 
  
-}
-
-int main() {
-    int elements[] = {9, 5, 3, 5, 6, 7, 9, 4};
-    int length = sizeof(elements) / sizeof(elements[0]);
-  
-    clock_t start = clock();
-  
-    bucketsort(elements, length);
-
     clock_t end = clock();
-  
-    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("after sroting: \n");
 
-    for(int i = 0; i < length; i++){
-        printf("%d ", elements[i]);
+    
+    double elapsed_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    
+    printf("Sorted Array:\n");
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+        printf("%d ", arr[i]);
     }
-
-    printf("\ntime taken: %f seconds\n", time_taken);
-
+    printf("\nElapsed time: %f s\n", elapsed_time);
+    free(arr);
     return 0;
 }
-
